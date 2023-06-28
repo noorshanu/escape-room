d1fa24511640a47950035efb9b26fca029d56b93
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-06-28 13:44:32 +0530

    basic things

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div> <div className='fixed max-w-[1300px] w-full bg-black navbar mx-auto flex justify-between '>
+    <div> <div className='fixed container-wrapper w-full bg-black navbar m-auto flex justify-between '>
       <div className='social flex'>
-
+<div>
+  media
+</div>
 
 
       </div>
--- src/section/About.jsx
+++ src/section/About.jsx
@@ -5,11 +5,14 @@ function About() {
     <>
       <img src="images/about.png" alt="" className="w-full" />
       <section className="pt-8 bg-[#b7e3ff]">
+        <div className="container-wrapper">
         <div className="flex">
           <div>
             <h1>This section is about Our works.</h1>
           </div>
         </div>
+        </div>
+       
       </section>
     </>
   );
