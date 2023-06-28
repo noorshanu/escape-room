b92af6ad92b5957d1fe67cc41c2cf22c98185f29
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-06-28 12:40:30 +0530

    Update About.jsx

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function About() {
   return (
     <>
-      <img src="images/about.png" alt="" />
-      <section className="pt-8">
+      <img src="images/about.png" alt="" className="w-full" />
+      <section className="pt-8 bg-[#b7e3ff]">
         <div className="flex">
           <div>
             <h1>This section is about Our works.</h1>
