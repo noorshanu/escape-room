6113580b9152867451c42c4d4eacccc06f2625b9
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-06-28 23:25:51 +0530

    partner images

--- src/section/Partners.jsx
+++ src/section/Partners.jsx
@@ -12,21 +12,21 @@ function Partners() {
             <div className="content-img mx-auto text-center">
               <div className="logo-grid">
                 <img src="images/part/AmazonPay.png" alt="" />
-                <img src="images/part/AmazonPay.png" alt="" />
-                <img src="images/part/AmazonPay.png" alt="" />
-                <img src="images/part/AmazonPay.png" alt="" />
-                <img src="images/part/AmazonPay.png" alt="" />
+                <img src="images/part/img2.png" alt="" />
+                <img src="images/part/img3.png" alt="" />
+                <img src="images/part/img4.png" alt="" />
+                <img src="images/part/img5.png" alt="" />
                
 
                 
                 
               </div>
               <div className="logo-grid">
-              <img src="images/part/AmazonPay.png" alt="" />
-                <img src="images/part/AmazonPay.png" alt="" />
-                <img src="images/part/AmazonPay.png" alt="" />
-                <img src="images/part/AmazonPay.png" alt="" />
-                <img src="images/part/AmazonPay.png" alt="" />
+              <img src="images/part/img6.png" alt="" />
+                <img src="images/part/img7.png" alt="" />
+                <img src="images/part/img8.png" alt="" />
+                <img src="images/part/img9.png" alt="" />
+                <img src="images/part/GooglePay.png" alt="" />
               </div>
             </div>
           </div>
