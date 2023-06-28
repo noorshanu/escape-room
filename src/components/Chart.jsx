8181483c8b9d352da27901d64ef41309f4600bab
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-06-29 02:25:17 +0530

    ui fixed

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 function Chart() {
   return (
-    <section className='pt-8 bg-[#b7e3ff]  pb-16 relative'>
+    <section className='pt-12 bg-[#b7e3ff]  pb-16 relative'>
+      <img src="images/icons/img1.png" alt="" className=' absolute top-[14%] left-[9%]'/>
+      <img src="images/icons/img2.png" alt="" className=' absolute top-[26%] right-[5%]'/>
+      <img src="images/icons/star.png" alt="" className=' absolute top-[46%] left-[3%]' />
         <div className='container-wrapper'>
             <div className='text-center mt-16 '>
-                <img src="images/chart.png" alt="" className='mx-auto h-[400px] z-10 relative'/>
+                <img src="images/chart.png" alt="" className='mx-auto  z-10 relative'/>
             </div>
 
         </div>
@@ -13,6 +16,8 @@ function Chart() {
 
         <img src="images/about-bg.png" alt=""  className='w-full mx-auto text-center'/>
         </div>
+        <img src="images/icons/star.png" alt="" className=' absolute top-[82%] right-[22%]' />
+        <img src="images/icons/half.png" alt="" className=' absolute top-[84%] left-0' />
     </section>
   )
 }
--- src/section/Roadmap.jsx
+++ src/section/Roadmap.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Roadmap() {
   return (
     <>
-      <img src="images/road.png" alt="" className="w-full" />
+      <img src="images/road.png" alt="" className="w-full relative z-10" />
       <div className='bg-[url("/public/images/cardsFrame.svg")] bg-cover'>
         <div className="flex max-w-7xl flex-wrap mx-auto pb-16 pt-52 lg:h-[800px] gap-x-[4%] px-10">
           <img src="/images/card1.svg" className="w-full lg:w-[22%] sm:w-[47%] h-auto place-self-start" />
