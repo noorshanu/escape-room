944578aad8c8ca407901eca5722ef38cf802680c
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-06-28 16:38:22 +0530

    Update About.jsx

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -6,7 +6,7 @@ function About() {
       <img src="images/about.png" alt="" className="w-full" />
       <section className="pt-8 bg-[#b7e3ff]">
         <div className="container-wrapper">
-        <div className="flex">
+        <div className="flex items-center">
           <div>
             <img src="images/where.svg" alt="" />
           </div>
