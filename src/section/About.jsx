1bf47bff29e9bcee05c25cb130a60d77a3b598f5
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-06-29 18:09:32 +0530

    marque

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import Marquee from "react-fast-marquee";
 function About() {
   return (
     <>
-      <img src="images/about.png" alt="" className="w-full" />
+     
+      <div className="bg-[#FF768C] pt-3 pb-3 ">
+        <Marquee>
+          <h1 className="text-[#fff] font-modak text-6xl txt2 overflow-hidden ">First AI & ZK Based Escape Room Game . First AI & ZK Based Escape Room Game</h1>
+        </Marquee>
+      </div>
       <section className="pt-8 bg-[#b7e3ff]">
         <div className="container-wrapper">
           <div className="flex items-center">
